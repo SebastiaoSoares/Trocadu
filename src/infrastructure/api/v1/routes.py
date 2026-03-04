a34502bfbcfffe95aa699ecf6274557100098459
Sabrina Alencar <alencar.sabrina@aluno.ufca.edu.br>
2026-03-03 21:28:08 -0300

    feat: seed word JSON and link to PacoteArquivo

--- src/infrastructure/api/v1/routes.py
+++ src/infrastructure/api/v1/routes.py
@@ -1,9 +1,20 @@
 import os
 from fastapi import APIRouter
 from src.domain.shared.factories import PartidaFactory
+from src.infrastructure.repositories.pacote_arquivo import PacoteArquivo
 
 router = APIRouter()
 
 @router.get("/")
 def health_check():
     return {"status": f"Trocadu API online! Ambiente: {os.getenv('ENV')}"}
+
+@router.post("/partida")
+def criar_partida(tipo: str):
+    caminho_json = os.path.join(os.getcwd(), "src", "data", "palavras.json")
+    
+    banco_de_palavras = PacoteArquivo(caminho_arquivo=caminho_json)
+    
+    partida = PartidaFactory.criar_partida(tipo, [], banco_de_palavras)
+    
+    return {"mensagem": "Partida criada usando palavras.json", "tipo": tipo}
